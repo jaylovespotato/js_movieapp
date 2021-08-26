0a4c389c7a366cc8e7f795d9b9628cea0ec0df50
jay <jjhjoa2494@kaist.ac.kr>
2021-08-26 11:49:01 +0900

    detail까지 구현

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,12 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const IMAGE_BASE_URL = 'https:image.tmdb.org/t/p/';
+
+
+export const API_KEY = 'fecfc649bd08321acb36ad200a2ec4ee';
+
+// API 읽기 토큰 : eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZWNmYzY0OWJkMDgzMjFhY2IzNmFkMjAwYTJlYzRlZSIsInN1YiI6IjYxMjVkOGQ2ZmRmOGI3MDA5M2U2ZGEyYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gnmK9y7CITaChN86JhgTvI8F9KjTWrsloBhZlXBEhfE
---
+++ client/src/components/views/MovieDetail/MovieDetail.js
@@ -0,0 +1,99 @@
+import { Row } from 'antd'
+import React, {useEffect, useState, Fragment} from 'react'
+import { API_KEY, API_URL, IMAGE_BASE_URL } from '../../Config'
+import GridCards from '../commons/GridCards'
+import MainImage from '../LandingPage/Sections/MainImage'
+import MovieInfo from './Sections/MovieInfo'
+
+function MovieDetail(props) {
+
+
+    const movieId = props.match.params.movieId
+    const [Movie, setMovie] = useState([])
+    const [Casts, setCasts] = useState([])
+    const [ToggleActor, setToggleActor] = useState(false)
+
+    useEffect(() => {
+        
+        const endpointCrew = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`
+        
+        const endpointInfo = `${API_URL}movie/${movieId}?api_key=${API_KEY}&language=en-US`
+
+        fetch(endpointInfo)
+        .then(response => response.json())
+        .then(response => {
+            
+                console.log(response)
+                setMovie(response)
+            })
+
+
+        fetch(endpointCrew)
+        .then(response => response.json())
+        .then(response => {
+            
+                console.log(response)
+                setCasts(response.cast)
+
+            })
+    
+        
+    }, [])
+
+    const handleToggle=(e)=>{
+        setToggleActor(!ToggleActor)
+    }
+    
+
+    return (
+        
+            <div style={{width: '100%', margin:'0'}}>
+                    {/* Header */}
+                {<MainImage 
+                    image={`${IMAGE_BASE_URL}w1280${Movie.backdrop_path}`}
+                    title={Movie.original_title}
+                    text={Movie.overview}
+                />
+                }
+
+
+            {/* Body */}
+            <div style={{width:'85%', margin: '1rem auto' }}>
+
+
+                {/* MovieInfo */}
+                <MovieInfo 
+                    movie = {Movie}
+                />
+
+                <br/>
+
+                {/* Actor Grid */}
+
+                <div style={{display:'flex', justifyContent:'center', margin:'2rem'}}>
+                    <button onClick={handleToggle}> Toggle Actor View</button>
+
+                </div>
+
+                {ToggleActor &&
+                    <Row gutter={[16,16]}>
+                    {Casts && Casts.map((cast, index) =>(
+                        <Fragment key={index}>
+
+                            <GridCards
+                                
+                                image={cast.profile_path?
+                                        `${IMAGE_BASE_URL}w500${cast.profile_path}` : null}
+                                    
+                                characterName={cast.name}
+                            />
+                        </Fragment>
+                    ))}
+                    </Row>
+                }
+            </div>
+        </div>
+    )
+}
+
+export default MovieDetail
